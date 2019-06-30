a289a4337e953c4875075182a512cbd90ec00527
nandohidayat <mnandohidayat@gmail.com>
2019-07-01 03:51:24 +0700

    new sign up login and splash

--- client/lib/pages/login.dart
+++ client/lib/pages/login.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-import 'package:client/models/user.dart';
-import 'package:toast/toast.dart';
+import 'package:client/pages/signup.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -22,175 +20,89 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        resizeToAvoidBottomPadding: false,
-        body: Column(
+      appBar: AppBar(
+        title: Text('Masuk'),
+        actions: <Widget>[
+          MaterialButton(
+            child: Text('Daftar'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SignupPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: 30,
+          horizontal: 20,
+        ),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
-                    child: Text('Toko',
-                        style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold)),
+            Text('Email'),
+            TextField(
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
                   ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
-                    child: Text('Buku',
-                        style: TextStyle(
-                            fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(210.0, 175.0, 0.0, 0.0),
-                    child: Text('.',
-                        style: TextStyle(
-                            fontSize: 80.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green)),
-                  )
-                ],
+                ),
+                focusedBorder: UnderlineInputBorder(),
               ),
             ),
-            Container(
-                padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'EMAIL',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
-                      controller: _emailController,
-                    ),
-                    SizedBox(height: 20.0),
-                    TextField(
-                      decoration: InputDecoration(
-                          labelText: 'PASSWORD',
-                          labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green))),
-                      obscureText: true,
-                      controller: _passwordController,
-                    ),
-                    SizedBox(height: 5.0),
-//                    Container(
-//                      alignment: Alignment(1.0, 0.0),
-//                      padding: EdgeInsets.only(top: 15.0, left: 20.0),
-//                      child: InkWell(
-//                        child: Text(
-//                          'Forgot Password',
-//                          style: TextStyle(
-//                              color: Colors.green,
-//                              fontWeight: FontWeight.bold,
-//                              fontFamily: 'Montserrat',
-//                              decoration: TextDecoration.underline),
-//                        ),
-//                      ),
-//                    ),
-                    SizedBox(height: 40.0),
-                    Container(
-                      height: 40.0,
-                      child: GestureDetector(
-                        onTap: () async {
-                          int status = await User().login(
-                              email: _emailController.text,
-                              password: _passwordController.text);
-
-                          if (status == 201) {
-                            Toast.show('Login successfull', context);
-
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => BooksHome()));
-                          } else {
-                            Toast.show(
-                                'Failed to login. Please try again later',
-                                context);
-                          }
-                        },
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20.0),
-                          shadowColor: Colors.greenAccent,
-                          color: Colors.green,
-                          elevation: 7.0,
-                          child: Center(
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat'),
-                            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Kata Sandi'),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: OutlineButton(
+                    padding: EdgeInsets.all(0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.black),
+                      padding: EdgeInsets.all(
+                        12,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Masuk',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(height: 20.0),
-//                    Container(
-//                      height: 40.0,
-//                      color: Colors.transparent,
-//                      child: Container(
-//                        decoration: BoxDecoration(
-//                            border: Border.all(
-//                                color: Colors.black,
-//                                style: BorderStyle.solid,
-//                                width: 1.0),
-//                            color: Colors.transparent,
-//                            borderRadius: BorderRadius.circular(20.0)),
-//                        child: Row(
-//                          mainAxisAlignment: MainAxisAlignment.center,
-//                          children: <Widget>[
-//                            Center(
-//                              child:
-//                              ImageIcon(AssetImage('assets/facebook.png')),
-//                            ),
-//                            SizedBox(width: 10.0),
-//                            Center(
-//                              child: Text('Log in with facebook',
-//                                  style: TextStyle(
-//                                      fontWeight: FontWeight.bold,
-//                                      fontFamily: 'Montserrat')),
-//                            )
-//                          ],
-//                        ),
-//                      ),
-//                    )
-                  ],
-                )),
-            SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'New to Toko Buku ?',
-                  style: TextStyle(fontFamily: 'Montserrat'),
-                ),
-                SizedBox(width: 5.0),
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/signup');
-                  },
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                        color: Colors.green,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline),
                   ),
-                )
+                ),
               ],
             )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
--- client/lib/pages/splash.dart
+++ client/lib/pages/splash.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:client/pages/login.dart';
+import 'package:client/pages/signup.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -9,13 +11,25 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  bool showButton = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
+//    Timer(
+//        Duration(seconds: 3), () => Navigator.of(context).pushNamed('/login'));
     Timer(
-        Duration(seconds: 3), () => Navigator.of(context).pushNamed('/login'));
+      Duration(seconds: 3),
+      () {
+        setState(
+          () {
+            showButton = true;
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -23,7 +37,111 @@ class _SplashPageState extends State<SplashPage> {
     // TODO: implement build
     return Scaffold(
       body: Center(
-        child: Image(image: AssetImage('assets/logo1.png'), fit: BoxFit.cover),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 3),
+                  curve: Curves.bounceIn,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'TOKO BUKU',
+                        style: TextStyle(
+                          fontFamily: 'MADE Coachella',
+                          fontSize: 12,
+                        ),
+                      ),
+                      Text(
+                        'Bu Ratna',
+                        style: TextStyle(
+                          fontFamily: 'MADE Coachella',
+                          fontSize: 40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: showButton,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: OutlineButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(3),
+                          ),
+                          padding: EdgeInsets.all(
+                            12,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Masuk',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: OutlineButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignupPage(),
+                            ),
+                          );
+                        },
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black),
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.black),
+                          padding: EdgeInsets.all(
+                            12,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Daftar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
