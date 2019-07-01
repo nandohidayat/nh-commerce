ea2e45aa1a62744790844bf8fe7a13f45e67fb59
nandohidayat <mnandohidayat@gmail.com>
2019-07-02 03:35:30 +0700

    fix something

--- admin/controllers/web/home.js
+++ admin/controllers/web/home.js
@@ -1,8 +1,8 @@
 export default class Index {
     static index(req, res) {
         res.render('default', {
-            title: 'Node commerce',
-            message: 'Welcome to Node Commerce project :)',
+            title: 'Toko Buku Bu Ratna',
+            message: 'Welcome :)',
             currentPath: req.baseUrl,
         });
     }
